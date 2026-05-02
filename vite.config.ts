497602d332133dd92baf9ca91707599cce314c90
Daniyar Kashkaraliev <daniyar.kashkaraliev@gmail.com>
2026-05-02 12:21:19 +0600

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [],
+  envDir:'.',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
